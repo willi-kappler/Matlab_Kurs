bcb3ad22513155e41fe6af4d6ad331899f58efa9
Willi Kappler <willi-kappler@users.noreply.github.com>
2016-03-10 17:02:02 +0100

    more slides for chapter 2, added scons builder for octave plots

---
+++ lecture2/example2_1.m
@@ -0,0 +1,4 @@
+x = 0:0.5:6;
+y = sqrt(x);
+plot(x,y);
+print -dpng lecture2/example2_1.png
--- site_scons/site_init.py
+++ site_scons/site_init.py
@@ -1,3 +1,7 @@
 def compile_matlab_code(env):
     generate_output = Builder(action = "octave -q $SOURCE > $TARGET", suffix = ".txt", src_suffix = ".m")
     env.Append(BUILDERS = {"MATLAB" : generate_output})
+
+def generate_matlab_plot(env):
+    generate_plot = Builder(action = "octave -q $SOURCE", suffix = ".png", src_suffix = ".m")
+    env.Append(BUILDERS = {"MATLAB_PLOT" : generate_plot})
