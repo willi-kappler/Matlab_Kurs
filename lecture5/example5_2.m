ed04dcf01b82058d92dccf9de8752b94e9ca9871
Willi Kappler <willi.kappler@uni-tuebingen.de>
2016-03-23 17:37:56 +0100

    finish chapter 5

---
+++ lecture5/example5_2.m
@@ -0,0 +1,10 @@
+function [out1,out2] = myfunction(in1,in2)
+  % Loest die Weltformel
+  % Verwendung:
+  % [out1,out2] = myfunction(in1,in2)
+  % in1: erster Eingabeparameter [Einheit]
+  % in2: zweiter Eingabeparameter [Einheit]
+  % out1: ...
+  % written by Max Mustermann,
+  % Maerz 30, 2016
+end
