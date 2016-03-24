06364b5566b4e18c982733b04b96df436aa09707
Christiane Zarfl <christiane.zarfl@gmx.de>
2016-03-24 10:42:48 +0100

    Aenderungen Vorlesung 1 bis 5

--- lecture3/example3_2.m
+++ lecture3/example3_2.m
@@ -1,5 +1,5 @@
-xvec = [-2:0.1:2]; yvec = [-1:0.1:1];
+xvec = [-3:0.1:3]; yvec = [-3:0.1:3];
 [X,Y] = meshgrid(xvec,yvec);
-Z = X.*Y;
+Z = cos(X.*Y);
 surf(X,Y,Z); % s. auch "doc surf"
 xlabel('x'); ylabel('y'); zlabel('z');
