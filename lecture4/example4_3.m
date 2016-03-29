7cae273dd0fb8bcc15ead3d7e938423f0decf517
Christiane Zarfl <christiane.zarfl@gmx.de>
2016-03-29 09:33:38 +0200

    Ergänzung und Überarbeitung der Vorlesungsfolien

--- lecture4/example4_3.m
+++ lecture4/example4_3.m
@@ -2,7 +2,7 @@ x=1;        % Initialisierung
             % Beginn der Schleife
 while x<10  % Fortsetzungsbedingung
             % diverse Operationen
-  x=x*1.2;  % oder irgendetwas anderes
+  x=x*1.2;  % oder irgendetwas anderes,
             % was Einfluss auf die
             % Bedingung hat
 end         % Ende der Schleife
